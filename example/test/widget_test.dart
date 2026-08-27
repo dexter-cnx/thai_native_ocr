42611c354a9dc7a1b5386c599fff7a3c0cc0115e
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-27 21:08:45 +0700

    test: fix generated example widget smoke test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,30 +1,11 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:thai_native_ocr_example/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('renders OCR example shell', (tester) async {
+    await tester.pumpWidget(const ThaiNativeOcrExampleApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('thai_native_ocr example'), findsOneWidget);
+    expect(find.text('Pick image and recognize'), findsOneWidget);
   });
 }
